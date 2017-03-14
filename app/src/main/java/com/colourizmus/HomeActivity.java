fa22295f23fe493fcf9ac10e128c01428d458edb
Simeon <simeon.zlatanov.andreev@abv.bg>
2017-03-14 06:14:30 +0200

    Added basic 'welcome' Activity and Sign In to be called when no sign-in in ShrdPrefs

--- app/src/main/java/com/colourizmus/HomeActivity.java
+++ app/src/main/java/com/colourizmus/HomeActivity.java
@@ -1,13 +1,80 @@
 package com.colourizmus;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+
+    public static SharedPreferences userInfo;
+    public static String name;
+    public static String gender;
+
+    Button proceedBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        TextView welcomeMessage = (TextView) findViewById(R.id.welcome_text_field);
+        proceedBtn = (Button) findViewById(R.id.proceed_button);
+
+        userInfo = getSharedPreferences("UserData", MODE_PRIVATE);
+        name = userInfo.getString("name", null);
+        gender = userInfo.getString("gender", null);
+
+
+        if (name == null || gender == null || name.isEmpty() || !(gender.equals(getString(R.string.lady)) || gender.equals(getString(R.string.lord)))) {
+            Toast.makeText(this, getString(R.string.notSignedIn), Toast.LENGTH_LONG).show();
+            Intent i = new Intent(this, SignInActivity.class);
+            startActivity(i);
+        }
+
+        name = userInfo.getString("name", "oh fuck...");
+        gender = userInfo.getString("gender", "oh fuck...");
+        //TODO - extract String literal to strings.xml for better flexibility!
+        welcomeMessage.setText("Welcome " + gender + " " + name + "!");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(this.toString(), "main.onStart(...)");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(this.toString(), "main.onResume(...)");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(this.toString(), "main.onPause(...)");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(this.toString(), "main.onStop(...)");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(this.toString(), "main.onDestroy(...)");
+
     }
 }
---
+++ app/src/main/java/com/colourizmus/SignInActivity.java
@@ -0,0 +1,117 @@
+package com.colourizmus;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class SignInActivity extends AppCompatActivity {
+
+    //User name and Shared prefs should maybe be passed in bundle or re-instantiated?
+    RadioGroup genderRadio;
+    Button proceedBtn;
+    EditText nameInput;
+
+    String name;
+    String gender;
+    SharedPreferences shPref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        Log.e(this.toString(), "SignIn.onCreate(...)");
+
+        shPref = getSharedPreferences("UserData", MODE_PRIVATE);
+
+        nameInput = (EditText) findViewById(R.id.nameInput);
+        genderRadio = (RadioGroup) findViewById(R.id.genderRadio);
+        proceedBtn = (Button) findViewById(R.id.proceedButton);
+
+        genderRadio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                //could be ternary oprtr I guess...
+                switch (checkedId) {
+                    case R.id.LadyRadio:
+                        gender = getString(R.string.lady);
+                        break;
+                    case R.id.LordRadio:
+                        gender = getString(R.string.lord);
+                        break;
+                    default:
+                        gender = "WHAAAAAAAAAAAAAAAA?!";
+                }
+            }
+        });
+
+
+        proceedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                name = nameInput.getText().toString();
+
+                if (gender == null || name == null) {
+                    Toast.makeText(SignInActivity.this, "Please answer both questions.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (name.isEmpty()) {
+                    Toast.makeText(SignInActivity.this, "Please enter thine name.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                SharedPreferences.Editor spEdit = shPref.edit();
+                spEdit.putString("name", name);
+                spEdit.putString("gender", gender);
+                spEdit.commit();
+
+                Intent i = new Intent(SignInActivity.this, HomeActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(this.toString(), "SignIn.onStart(...)");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(this.toString(), "SignIn.onResume(...)");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(this.toString(), "SignIn.onPause(...)");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(this.toString(), "SignIn.onStop(...)");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(this.toString(), "SignIn.onDestroy(...)");
+
+    }
+}
