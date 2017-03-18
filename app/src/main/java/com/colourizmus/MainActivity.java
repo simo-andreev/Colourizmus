fe13ce7dd01a8c639873fee48a23d3405bdf8b99
simmo-andreev <simeon.zlatanov.andreev@abv.bg>
2017-03-19 01:29:33 +0200

    I honestly have no idea what I've done o_O. I've been testing some things and played around a bit, nothing crucial should've been changed. Anyway... yeah... sorry future me

--- app/src/main/java/com/colourizmus/MainActivity.java
+++ app/src/main/java/com/colourizmus/MainActivity.java
@@ -86,40 +86,6 @@ public class MainActivity extends AppCompatActivity implements PickerFragment.On
     }
 
 
-//    public static class PlaceholderFragment extends Fragment {
-//        /**
-//         * The fragment argument representing the section number for this
-//         * fragment.
-//         */
-//        private static final String ARG_SECTION_NUMBER = "section_number";
-//
-//        public PlaceholderFragment() {
-//        }
-//
-//        /**
-//         * Returns a new instance of this fragment for the given section
-//         * number.
-//         */
-//        public static PlaceholderFragment newInstance(int sectionNumber) {
-//            PlaceholderFragment fragment = new PlaceholderFragment();
-//
-//            Bundle args = new Bundle();
-//            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-//
-//            fragment.setArguments(args);
-//            return fragment;
-//        }
-//
-//        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//
-//            View rootView = inflater.inflate(R.layout.activity_colour_creator, container, false);
-//
-//            return rootView;
-//        }
-//    }
-
-
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
