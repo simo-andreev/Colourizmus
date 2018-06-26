fb921ce6b4f3c629c4cb2d44da50657b56865c41
simo-andreev <simeon.zlatanov.andreev@abv.bg>
2018-06-27 02:17:25 +0300

    fix: [0000004] - update test for AppContext package name to reflect the changed application domain form 'com' to 'sim.o.bg' (which was changed about half an year ago. woops)

--- app/src/androidTest/java/bg/o/sim/colourizmus/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/bg/o/sim/colourizmus/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.colourizmus", appContext.getPackageName());
+        assertEquals("bg.o.sim.colourizmus", appContext.getPackageName());
     }
 }
